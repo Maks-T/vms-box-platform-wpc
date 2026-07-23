edd0b81c902571f93ba4609b2835cd68ac80a23e
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2026-07-23 18:42:36 +0300

    update: core

--- deploy-hook.sh
+++ deploy-hook.sh
@@ -5,12 +5,12 @@ BRANCH_NAME=${1:-"deploy/build"}
 
 echo "Обновление статических файлов виджета калькулятора (Ветка: $BRANCH_NAME)..."
 
-rm -rf public/widget
+rm -rf public/calculator-app
 
-git clone -b "$BRANCH_NAME" --single-branch git@github.com:kapitulin24/projectstroy.git public/widget
+git clone -b "$BRANCH_NAME" --single-branch git@github.com:kapitulin24/projectstroy.git public/calculator-app
 
-rm -rf public/widget/.git
+rm -rf public/calculator-app/.git
 
-chmod -R 775 public/widget
+chmod -R 775 public/calculator-app
 
 echo "Виджет успешно обновлен."
